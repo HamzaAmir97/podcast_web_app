375e7f6206c5d45108765fcc274230240f234e13
Hmazah <alhamza2012@gmail.com>
2025-10-15 14:51:17 +0300

    build media player

--- frontend/components/Core/SideBarPlayer.tsx
+++ frontend/components/Core/SideBarPlayer.tsx
@@ -5,9 +5,12 @@ const SideBarPlayer = () => {
     <div className='fixed right-0 '>
     <div
     className='relative w-[424px] p-10 h-[820px] bg-primary
-    flex flex-col gap-10 items-center  
+    flex flex-col gap-10 items-center   
     '
     >
+
+
+
 
         {/* logo */}
         <div className='pb-15'>
@@ -21,8 +24,14 @@ const SideBarPlayer = () => {
     </div>
     
 
+
+
+
     {/* podcast player */}
-    <div className=' absolute top-[187px] left-[64px] w-[296px] h-[346px]  bg-[#9F75FF]  border-dashed border-[2px]
+
+    {/* podcast player container */}
+    <div>
+    <div className=' absolute top-[160px] left-[70px] w-[296px] h-[346px]  bg-[#9F75FF]  border-dashed border-[2px]
      rounded-[24px] flex justify-center items-center border-[#E6E8EB]  opacity-30
     '>
 
@@ -30,20 +39,87 @@ const SideBarPlayer = () => {
 
     </div>
 
-    <p className=' absolute top-[340px] left-[136px]     text-white text-center '>Selecione um <br/>
+    <p className=' absolute top-[320px] left-[155px]     text-white text-center '>Selecione um <br/>
 podcast para ouvir</p>
+     
+
     </div>
 
 
 
+  {/* podcast player progress and controls */}
+    
+    
+     <div>
+     {/* podcast player progress */}
+     <div className='absolute top-[550px] left-[80px]
+     flex justify-center items-center gap-3
+     '>
+       <label>00:00</label>
+       <input type="range"
+       min={0}
+       max={100}
+       className='w-[174px] h-[4px] [accent-color:#13c2a3]'
+       />
+       <label>00:00</label>
+     </div>
+
     {/* podcast player controls */}
-    {/* <div className='w-[296px] h-[346px]  bg-[#9F75FF]  border-dashed border-[2px]
-     rounded-[24px] flex justify-center items-center border-[#E6E8EB]  opacity-50
+    <div className='
+    absolute
+    top-[610px] left-[90px]
+    w-[256px] h-[64px]   
+      flex justify-center items-center gap-5  opacity-50
+   cursor-pointer
     '>
-
   
+    <Image
+    src="/icons/shuffle.png"
+    alt="Logo"
+    width={24}
+    height={24}
+    className=' hover:bg-[#6F48C9] hover:rounded-[16px]'
+    />
+    <Image
+    src="/icons/play_right_arrow.png"
+    alt="Logo"
+    width={24}
+    height={24}
+    className=' hover:bg-[#6F48C9] hover:rounded-[16px]'
+    />
+    <div className='
+    flex justify-center items-center
+    w-[64px] h-[64px] rounded-[16px] bg-[#9164FA] hover:bg-[#6F48C9] hover:rounded-[16px]'>
+    <Image
+    src="/icons/play_arrow.png"
+    alt="Logo"
+    width={32}
+    height={32}
+    className=' '
+    />
+    </div>
+    <Image
+    src="/icons/play_left_arrow.png"
+    alt="Logo"
+    width={24}
+    height={24}
+    className=' hover:bg-[#6F48C9] hover:rounded-[16px]'
+    />
+    <Image
+    src="/icons/repeat.svg"
+    alt="Logo"
+    width={24}
+    height={24}
+    className=' hover:bg-[#6F48C9] hover:rounded-[16px]'
+    />
 
-    </div> */}
+    </div>
+ 
+    </div>
+ 
+
+
+ </div>
 
 
     </div>
